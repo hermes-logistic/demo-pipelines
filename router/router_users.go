8926fa2118dc269eab920ff5acf34700caddc1e2
tventura-hermes <tventura@hermesv.io>
2024-09-11 13:23:23 -0600

    read users

--- router/router_users.go
+++ router/router_users.go
@@ -2,11 +2,15 @@ package router
 
 import (
 	users_create_infrastructure "go-api/api/users/infrastructure/create"
+	users_read_infrastructure "go-api/api/users/infrastructure/read"
 
 	"github.com/gin-gonic/gin"
 )
 
 func (ro *Routes) UsersRoutes(r *gin.Engine, db interface{}) {
-	ur := users_create_infrastructure.NewUserData(db)
-	r.POST("/users", ur.CreateData)
+	ur := users_read_infrastructure.NewTaskHandler(db)
+	r.GET("/users", ur.ReadData)
+
+	uc := users_create_infrastructure.NewUserData(db)
+	r.POST("/users", uc.CreateData)
 }
